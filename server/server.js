034e6d40236f4cd3492c0fa478147c961f7b797a
wissem-thinklab <wissemchihaoui@thinklab.tn>
2026-02-04 16:00:01 +0100

    undo

--- server/server.js
+++ server/server.js
@@ -37,7 +37,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../public')));
   
   // Handle React routing, return all requests to React app
-  app.get('/*', (req, res) => {
+  app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../public', 'index.html'));
   });
 }
